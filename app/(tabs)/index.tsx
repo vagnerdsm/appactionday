9a7f05706ad9fa8e8019574804bdce8f82f5e626
OvelhaManca <vagner.matos47@gmail.com>
2024-02-07 15:01:33 -0300

    primeiro commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
+    <View className="bg-slate-200">
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
