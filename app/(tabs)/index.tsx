d09c96eb94c4f8ad9b42894effc294f49f098a06
OvelhaManca <vagner.matos47@gmail.com>
2024-02-07 16:02:52 -0300

    remove arquivos inúteis

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View >
- 
+      
 
     </View>
   );
---
+++ app/login.tsx
@@ -0,0 +1,19 @@
+
+
+import React, { Component } from 'react'
+import { Pressable, Text, TextInput, View } from 'react-native'
+
+export default class login extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
+
+
+
+
+
+
