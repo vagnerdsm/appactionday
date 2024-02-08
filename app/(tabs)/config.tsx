5da1539f7001f40fa2e54ef38d6ac4a0aa4c56c0
VitorKokkonen <vitorks08@outlook.com>
2024-02-08 16:09:53 -0300

    Estrutura cabeçalho e tabview

---
+++ app/(tabs)/config.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import React from 'react'
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab Two</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+});
