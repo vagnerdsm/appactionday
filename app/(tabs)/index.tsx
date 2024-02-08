8c040f4eabdfd8b47cddcc4834f1e570ca6b4eea
OvelhaManca <vagner.matos47@gmail.com>
2024-02-08 17:54:40 -0300

    adiciona o supabase, componente de auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { Pressable, StyleSheet, TextInput } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import React from 'react';
 
 export default function TabOneScreen() {
   return (
