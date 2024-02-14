66f039d31d4ef19c79f9203f1859be7ba8682972
OvelhaManca <vagner.matos47@gmail.com>
2024-02-14 17:57:19 -0300

    adiciona nome e email dinamico tela de config, imagem pendente

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -1,35 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View } from '@/components/Themed';
 import { Button, Image, TouchableOpacity } from 'react-native';
-import { getAuth, signOut } from 'firebase/auth';
-import { auth } from '../../firebaseConfig';
+import { auth, db } from '../../firebaseConfig';
 import { router } from 'expo-router';
 import { StyleSheet } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
 
 export default function TabConfig() {
-  const imageProfile = auth.currentUser?.photoURL ? auth.currentUser?.photoURL : require('../../assets/images/rdicon.png')
+  // const imageProfileUrl = userInfo?.photoURL ? userInfo?.photoURL : require('../../assets/images/rdicon.png')
   const username = auth.currentUser?.displayName
   const useremail = auth.currentUser?.email
-  
+  const [userInfo, setUserInfo] = useState<any | undefined>(null);
+  const useruid = auth.currentUser?.uid
+  // const imageProfileUrl = userInfo?.photoURL ? userInfo?.photoURL : require('../../assets/images/rdicon.png')
+  const [imagePlace, setImage] = useState("");
+
   const signOut = async () => {
-      try {
-        const response = await auth.signOut();
-        console.log(response)
-        router.replace("/login")
-      } catch (error) {
-        console.log(error)
-      }
-    };
+    try {
+      const response = await auth.signOut();
+      console.log(response)
+      router.replace("/login")
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  const fetchData = async () => {
+    const docRef = doc(db, "users", "SysT7VM9tMfEpHmmEXcSOpT5nHr2");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setUserInfo(docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      setUserInfo(doc.data());
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+    // getData();
+  }, []);
+
 
   return (
     <View style={styles.background}>
       <View style={styles.container}>
 
-        <Image style={styles.imageProfile} source={require('../../assets/images/rdicon.png')} />
+        <Image style={styles.imageProfile} source={{ uri: `${userInfo.photoURL}`}} />
 
-        <Text style={styles.textHeader}> Unicruz </Text>
-        <Text style={styles.textEmail}> setordeti@actiondaydigital.page </Text>
+        {/* {imagePlace ? (
+          <Image source={{ uri: `${userInfo.photoURL}` }} style={styles.imageProfile} />
+        ) : (
+          <></>
+        )} */}
+
+
+
+        <Text style={styles.textHeader}> {userInfo?.display_name} </Text>
+        <Text style={styles.textEmail}> {userInfo?.email} </Text>
 
         <View style={styles.optionsMenu}>
           <TouchableOpacity style={styles.buttonMenu}>
@@ -45,22 +84,22 @@ export default function TabConfig() {
               <MaterialIcons name="email" size={24} color="white" />
             </View>
           </TouchableOpacity>
-          
+
           <TouchableOpacity style={styles.buttonMenu}>
             <View style={styles.buttonContent}>
               <Text style={styles.buttonText}> Altere sua Senha </Text>
               <MaterialIcons name="password" size={24} color="white" />
             </View>
           </TouchableOpacity>
-          
-          <TouchableOpacity style={styles.buttonMenuLogout}>
+
+          <TouchableOpacity style={styles.buttonMenuLogout} onPress={signOut}>
             <View style={styles.buttonContentLogout}>
               <Text style={styles.buttonText}> Logout </Text>
               <MaterialIcons name="logout" size={24} color="white" />
             </View>
           </TouchableOpacity>
         </View>
-       
+
       </View>
     </View>
   );
@@ -68,14 +107,14 @@ export default function TabConfig() {
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: '#e5e5e5',
+    backgroundColor: '#19173D',
     flex: 1,
   },
   container: {
     marginTop: 35,
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#e5e5e5',
+    backgroundColor: '#19173D',
     // color: '#FFF',
     // justifyContent: 'center',
     // width: '90%'
@@ -106,7 +145,7 @@ const styles = StyleSheet.create({
     height: 300,
     marginTop: 20,
     gap: 16,
-    backgroundColor: '#e5e5e5',
+    backgroundColor: '#19173D',
   },
   buttonMenu: {
     width: '100%',
@@ -115,7 +154,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#7B78AA',
     justifyContent: 'center',
     paddingHorizontal: 10,
-    
+
     // alignItems: 'center',
   },
   buttonText: {
@@ -134,13 +173,13 @@ const styles = StyleSheet.create({
   buttonContent: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent:'space-between', 
+    justifyContent: 'space-between',
     backgroundColor: '#7B78AA',
   },
   buttonContentLogout: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent:'space-between', 
+    justifyContent: 'space-between',
     backgroundColor: '#871919',
   }
 });
