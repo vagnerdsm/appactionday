e381f7a601d865bf425df4066d6b052b7760163b
VitorKokkonen <vitorks08@outlook.com>
2024-02-15 17:54:41 -0300

    Tela relatorio Comercial

---
+++ components/(tabs-relatorios)/RealtorioComercial/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { StyleSheet, View, Text, ScrollView } from 'react-native'
+import Card from '@/components/Card';
+import ChartPie from '@/components/PieChart';
+
+const ThirdRoute = () => {
+    return (
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+            <View style={styles.rowContainer}>
+                <Card
+                    icon="check-square-o"
+                    name="Oportunidades Ganhas"
+                    value="R$400,00"
+                    iconColor="#61DE70"
+                />
+                <Card
+                    icon="window-close-o"
+                    name="Oportunidades Perdidas"
+                    value="R$400,00"
+                    iconColor="#851b20"
+                />
+            </View>
+
+            <View style={styles.rowContainer}>
+                <Card
+                    icon="rotate-right"
+                    name="Oportunidades em Andamento"
+                    value="R$400,00"
+                    iconColor="#665d5d"
+                />
+            </View>
+
+            <View style={styles.rowContainer}>
+                <ChartPie
+                    label='Vendedor 1'
+                />
+            </View>
+
+
+
+        </ScrollView >
+    )
+};
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+
+    rowContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 12,
+        paddingTop: 16,
+    },
+
+    tabBar: {
+        backgroundColor: '#fff',
+        borderColor: 'blue'
+    },
+
+    graphStyle: {
+        marginVertical: 8,
+        borderRadius: 16,
+    },
+
+});
+
+export default ThirdRoute 
