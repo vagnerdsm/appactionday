11556fa4f7e02de2c16d820f68cfad1d73883c80
VitorKokkonen <vitorks08@outlook.com>
2024-03-08 17:00:42 -0300

    Ajustes nos imports

---
+++ components/index.tsx
@@ -0,0 +1,9 @@
+import ChartBar from "./BarChart";
+import ChartPie from "./PieChart";
+import Card from "./Card";
+import MetaCard from "./MetaCard";
+import FirstRoute from "./(tabs-relatorios)/RelatorioGeral";
+import SecondRoute from "./(tabs-relatorios)/RelatorioDigital";
+import ThirdRoute from "./(tabs-relatorios)/RealtorioComercial";
+
+export { ChartBar, ChartPie, Card, MetaCard, FirstRoute, SecondRoute, ThirdRoute }
