f35788325ea89ce87cb46e119143e9d6ea721adf
VitorKokkonen <vitorks08@outlook.com>
2024-04-02 15:32:04 -0300

    Adicionando arquivos dos servicos para painel Adm

---
+++ app/src/services/servicesAdm/apiServiceBoleto.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const apiRequestBoleto = async () => {
+
+    const url = `https://appaction.vercel.app/api/boletos?cliente=URI SANTO ANGELO`;
+
+    try {
+        const response = await axios.get(url);
+
+        return response.data;
+    } catch (error) {
+        console.error('Erro ao fazer a solicitação:', error);
+    }
+};
+
+export default apiRequestBoleto;
