21147d7bbf553e4b3b3f9ce3f4a828ce26585bfa
VitorKokkonen <vitorks08@outlook.com>
2024-04-02 15:31:09 -0300

    Criando servico de api para os boletos do painel ADM

--- app/(tabs)/administrativo.tsx
+++ app/(tabs)/administrativo.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
-import { Pressable, StyleSheet, TextInput, View, Text } from 'react-native';
-
+import { FlatList, StyleSheet, TextInput, View, Text } from 'react-native';
+import userApiServiceBoleto from '../src/services/servicesAdm/useApiServiceBoleto';
 
 export default function TabOneScreen() {
+  const { data, isLoading } = userApiServiceBoleto()
+
   return (
-    <View>
-      <Text>Adms</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          { key: 'Dominic' },
+          { key: 'Jackson' },
+          { key: 'James' },
+          { key: 'Joel' },
+          { key: 'John' },
+          { key: 'Jillian' },
+          { key: 'Jimmy' },
+          { key: 'Julie' },
+        ]}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+      />
     </View>
   );
 }
-
-
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    borderColor: 'blue',
+    height: '100%'
+  },
+});
