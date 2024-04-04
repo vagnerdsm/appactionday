88e8392f4a06fc53f27561bdafb5d408f93d812f
VitorKokkonen <vitorks08@outlook.com>
2024-04-04 11:38:11 -0300

    Adicionando icones ao painel ADM

--- app/(tabs)/administrativo.tsx
+++ app/(tabs)/administrativo.tsx
@@ -1,18 +1,44 @@
 import React from 'react'
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FlatList, StyleSheet, View, Text, Image } from 'react-native';
 import userApiServiceBoleto from '../src/services/servicesAdm/useApiServiceBoleto';
 
 export default function TabOneScreen() {
   const { data, isLoading } = userApiServiceBoleto()
 
-  const DATA = data.map((item: { ID_ACTION: any; SERVICO: any; VALOR: any }) => ({ id: item.ID_ACTION, title: item.SERVICO }))
+  const iconMapping: any = {
+    " Cloudia": require('../../assets/icons/cloudia.png'),
+    "RD STATION": require('../../assets/icons/rdstation.png'),
+    "Serviço Action Day": require('../../assets//icons/actionLogo.png'),
+  }
 
-  type ItemProps = { title: string }
+  const DATA = data.map((item:
+    {
+      ID_ACTION: any;
+      SERVICO: any;
+      VENCIMENTO: any;
+      VALOR: any;
+    }) =>
+  ({
+    id: item.ID_ACTION,
+    title: item.SERVICO,
+    vencimento: item.VENCIMENTO.value,
+    value: item.VALOR,
+    icon: iconMapping[item.SERVICO]
+  }));
 
-  const Item = ({ title }: ItemProps) => (
+  type ItemProps = { title: string, value: string, vencimento: string, icon: any }
+
+  const Item = ({ title, value, vencimento, icon }: ItemProps) => (
+
     <View style={styles.item}>
-      <Text>{title}</Text>
-    </View>
+      <Image source={icon} style={styles.icon} />
+
+      <View style={styles.itemContent}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.valor} >Vencimento: {vencimento}</Text>
+        <Text style={styles.valor}>Valor: {value}</Text>
+      </View>
+    </View >
   );
 
   return (
@@ -20,7 +46,13 @@ export default function TabOneScreen() {
 
       <FlatList
         data={DATA}
-        renderItem={({ item }) => <Item title={item.title} />}
+        renderItem={({ item }) =>
+          <Item
+            title={item.title}
+            value={item.value}
+            vencimento={item.vencimento}
+            icon={item.icon}
+          />}
         keyExtractor={(item) => item.id}
       />
 
@@ -31,14 +63,37 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
 
   item: {
-    backgroundColor: '#ccc',
-    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 30,
     marginVertical: 8,
     marginHorizontal: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
   },
+
+  itemContent: {
+    flexDirection: 'column'
+  },
+
+  title: {
+    fontSize: 20,
+  },
+
+  valor: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  icon: {
+    width: 60,
+    height: 30,
+    marginRight: 10,
+  }
 
 });
