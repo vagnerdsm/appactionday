1cf502c081460d049ec9d15edd5c7f513dcf1be4
VagnerMatos <vagner.matos47@gmail.com>
2024-04-10 18:01:53 -0300

    implementando a autenticação supabase

---
+++ app/src/types/userMetadata.ts
@@ -0,0 +1,9 @@
+export interface UserMetadataType {
+    client: string | null;
+    crm: string | null;
+    display_name: string | null;
+    facebook_ads_id: string | null;
+    google_ads_id: string | null;
+    photo_url: string | null;
+    squad: string | null;
+  }
